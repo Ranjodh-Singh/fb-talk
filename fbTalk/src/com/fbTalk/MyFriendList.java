3cc0b7736abde5082d60d910009f65a152e02d56
ranjodh.87 <ranjodh.87@1ec80ec4-97da-9f5f-3abb-2552123dae40>
2012-09-11 09:18:26 +0000

    UPDATED FOR NULLPOINTER EXCEPTION

--- fbTalk/src/com/fbTalk/MyFriendList.java
+++ fbTalk/src/com/fbTalk/MyFriendList.java
@@ -4,19 +4,28 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MyFriendList extends Activity {
 
-	TextView questionCounter= (TextView)findViewById(R.id.questionCounter);
-	TextView questionText= (TextView)findViewById(R.id.questionText);
-	RadioGroup radioGroup = (RadioGroup)findViewById(R.id.radioGroup1);
-	Button nextButton = (Button)findViewById(R.id.nextButton);
+	TextView questionCounter;
+	TextView questionText;
+	RadioGroup radioGroup ;
+	Button nextButton;
+	Chronometer quizTimer;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_friend_list);
+        quizTimer=(Chronometer)findViewById(R.id.quizClockCounter);
+        quizTimer.start();
+        questionCounter= (TextView)findViewById(R.id.questionCounter);
+         questionText= (TextView)findViewById(R.id.questionText);
+    	 radioGroup = (RadioGroup)findViewById(R.id.radioGroup1);
+    	 nextButton = (Button)findViewById(R.id.nextButton);
+    	 
     }
 
     @Override
@@ -24,4 +33,5 @@ public class MyFriendList extends Activity {
         getMenuInflater().inflate(R.menu.activity_my_friend_list, menu);
         return true;
     }
+    
 }
